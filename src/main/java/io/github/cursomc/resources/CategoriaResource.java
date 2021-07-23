7e6782a15b643e15352133930994453d36624592
Rafael <rafael.2018.ads@gmail.com>
2021-07-23 09:46:40 -0300

    Testando a primeira classe de domínio -Categoria

--- src/main/java/io/github/cursomc/resources/CategoriaResource.java
+++ src/main/java/io/github/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package io.github.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value= "/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@GetMapping
-	public String listar() {
-		return "Funcionando";
+	public List listar() {
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+
+		List<Categoria> lista = new ArrayList<>();
+
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
 
 }
